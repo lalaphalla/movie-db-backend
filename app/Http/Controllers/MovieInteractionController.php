18056ba3d73ac9a9c225441be28ee30d2bbf52a0
Phalla <lalaphalla@gmail.com>
2025-09-02 14:48:28 +0700

    update: movie favorite and api route

--- app/Http/Controllers/MovieInteractionController.php
+++ app/Http/Controllers/MovieInteractionController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class MovieInteractionController extends Controller
 {
-    //
+    public function addFavorite($movieId)
+    {
+        $user = Auth::user();
+
+        $favorite = Favorite::firstOrCreate([
+            'user_id' => $user->id,
+            'movie_id' => $movieId,
+        ]);
+
+        return response()->json([$favorite, 201]);
+    }
+
+    public function userFavorites($userId)
+    {
+        $favorites = Favorite::where('user_id', $userId)->get();
+
+        return response()->json($favorites);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieInteractionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,10 @@ Route::post('/users', [UserController::class, 'store']);
 Route::post('/register', [AuthController::class, 'AuthController@register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// Like
+// Route::post('/movies/{id}/favorites', [MovieInteractionController::class, 'addFavorite']);
+Route::middleware('auth:sanctum')->post('/movies/{id}/favorites', [MovieInteractionController::class, 'addFavorite']);
+
+// User Like & Favorite
+Route::get('/users/{id}/favorites', [MovieInteractionController::class, 'userFavorites']);
