66b2c208ffc9a6fd0da4820a90d7613ae29719d6
Phalla <lalaphalla@gmail.com>
2025-09-02 14:05:06 +0700

    add: Like and favorite  model

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
